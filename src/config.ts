71b38b9cd37ee69153d90b231dc7adae75aeee2b
livingfree2023 <125038641+livingfree2023@users.noreply.github.com>
2026-06-30 21:09:55 +0800

    change site title

--- src/config.ts
+++ src/config.ts
@@ -8,8 +8,8 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "鸡鸡复鸡鸡",
-	subtitle: "瞎鸡鸡买鸡",
+	title: "天堂之路",
+	subtitle: "HeavenRoad",
 	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 200, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
